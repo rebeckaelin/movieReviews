37a489ec2d8c7bcf7179612f3faa6bd89b93bd57
rebeckaelin <rebeckaelin@hotmail.com>
2024-06-03 13:39:37 +0200

    added postman documentation

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ app.use("/api", userRoutes);
 app.use("/api", movieRoutes);
 app.use("/api", reviewRoutes);
 
-mongoose
-  .connect("mongodb://localhost:27017/movieReviews")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error: ", err));
+// mongoose
+//   .connect("mongodb://localhost:27017/movieReviews")
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error: ", err));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+const startServer = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/movieReviews");
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("MongoDB connection error: ", err);
+  }
+};
+
+startServer();
